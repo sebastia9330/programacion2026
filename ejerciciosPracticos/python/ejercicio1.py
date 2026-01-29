bd238d58f9e8281291f67d239bef27461576e361
sebastian carrero <mugssebastian@gmail.com>
2026-01-29 11:06:15 -0500

    Add gitignore and update Python exercises and LangChain notebook

--- ejerciciosPracticos/python/ejercicio1.py
+++ ejerciciosPracticos/python/ejercicio1.py
@@ -1,3 +1,8 @@
+
+from datetime import datetime
+from collections import defaultdict
+
+
 ventas = [
     {"cliente": "Ana", "monto": "120.50", "fecha": "2026-01-20", "pais": "CO"},
     {"cliente": "Luis", "monto": "85", "fecha": "2026/01/21", "pais": "CO"},
@@ -6,3 +11,60 @@ ventas = [
     {"cliente": "Luis", "monto": "error", "fecha": "2026-01-23", "pais": "CO"},
     {"cliente": "Carlos", "monto": "99.99", "fecha": "2026-01-23", "pais": None},
 ]
+
+print(ventas)
+
+formatos_fecha = [
+    "%Y-%m-%d",
+    "%Y/%m/%d",
+    "%d-%m-%Y",
+]
+
+ventasLimpias = []
+
+for venta in ventas:
+    try:
+        monto = float(venta['monto'])
+    except(TypeError, ValueError):
+        continue
+
+    fechaNormalizada = None
+    for fmt in formatos_fecha:
+        try:
+            fechaNormalizada = datetime.strptime(venta['fecha'], fmt).strftime('%Y-%m-%d')
+            break
+        except ValueError:
+            pass
+
+    if fechaNormalizada is None:
+        continue
+
+    pais = venta['pais'] if venta['pais'] is not None else 'DESCONOCIDO'
+
+    ventasLimpias.append({
+        'cliente': venta['cliente'],
+        'monto': monto,
+        'fecha': fechaNormalizada,
+        'pais': pais
+    })
+
+print(ventasLimpias)
+
+
+# Total de ventas válidas
+total_ventas = sum(v["monto"] for v in ventasLimpias)
+
+# Venta promedio
+venta_promedio = total_ventas / len(ventasLimpias)
+
+# Total por país
+ventas_por_pais = defaultdict(float)
+for v in ventasLimpias:
+    ventas_por_pais[v["pais"]] += v["monto"]
+
+# Cliente que más gastó
+gasto_por_cliente = defaultdict(float)
+for v in ventasLimpias:
+    gasto_por_cliente[v["cliente"]] += v["monto"]
+
+cliente_top = max(gasto_por_cliente, key=gasto_por_cliente.get)
